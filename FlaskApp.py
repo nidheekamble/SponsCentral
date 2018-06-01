53d9c41c7d36d4b6e25a4b18c7d4c780c2831d78
shrey-c <shreyansh.chheda@gmail.com>
2018-06-01 21:26:23 +0530

    files added in venv, removed posts=posts in flaskApp, renamed Templates to templates, made new folder 'static to store css'

--- FlaskApp.py
+++ FlaskApp.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route("/about")
--- Testfiles/base.py
+++
@@ -1,3 +0,0 @@
-#creating files because they need to be created. 
-#think of this as a primary (base) file for flask?
-#please add more.
