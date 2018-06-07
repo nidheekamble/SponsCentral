e96e80f908406f3a5dfb842a9c753ceaf52c9ca4
Nidhee Kamble <38733826+nidheekamble@users.noreply.github.com>
2018-06-07 16:12:41 +0530

    Testing SHA [run 3]
    Parenthesis

--- SponsCentral/routes.py
+++ SponsCentral/routes.py
@@ -122,9 +122,9 @@ def login():
         for char in (form.password.data):
             a = ord(char)
             s = s+a
-        now_hash = (str)((hashlib.sha512(((str(s)).encode('utf-8'))+((form.password.data).encode('utf-8'))).hexdigest()
+        now_hash = (str)((hashlib.sha512((str(s).encode('utf-8'))+((form.password.data).encode('utf-8')))).hexdigest())
         #if user and bcrypt.check_password_hash(user.password, form.password.data):
-        if (user and  (user.password==now_hash)):
+        if (user && (user.password==now_hash)):
 
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
